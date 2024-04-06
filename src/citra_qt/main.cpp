Lime3DS/Lime3DS

Update qt window title to use the long game title rather than short game title.

src/lime_qt/main.cpp
@@ -1238,8 +1238,11 @@ bool GMainWindow::LoadROM(const QString& filename) {
     }
 
     std::string title;
+    std::string title_long;
     system.GetAppLoader().ReadTitle(title);
+    system.GetAppLoader().ReadTitleLong(title_long);
     game_title = QString::fromStdString(title);
+    game_title_long = QString::fromStdString(title_long);
     UpdateWindowTitle();
 
     game_path = filename;
@@ -3054,14 +3057,14 @@ void GMainWindow::OnMoviePlaybackCompleted() {
 void GMainWindow::UpdateWindowTitle() {
     const QString full_name = QString::fromUtf8(Common::g_build_fullname);
 
-    if (game_title.isEmpty()) {
+    if (game_title_long.isEmpty()) {
         setWindowTitle(QStringLiteral("Lime %1").arg(full_name));
     } else {
-        setWindowTitle(QStringLiteral("Lime %1 | %2").arg(full_name, game_title));
+        setWindowTitle(QStringLiteral("Lime %1 | %2").arg(full_name, game_title_long));
         render_window->setWindowTitle(
-            QStringLiteral("Lime %1 | %2 | %3").arg(full_name, game_title, tr("Primary Window")));
+            QStringLiteral("Lime %1 | %2 | %3").arg(full_name, game_title_long, tr("Primary Window")));
         secondary_window->setWindowTitle(
-            QStringLiteral("Lime %1 | %2 | %3").arg(full_name, game_title, tr("Secondary Window")));
+            QStringLiteral("Lime %1 | %2 | %3").arg(full_name, game_title_long, tr("Secondary Window")));
     }
 }
 

yeager/Borked3DS

src/citra_qt/main.cpp
@@ -1213,8 +1213,11 @@ bool GMainWindow::LoadROM(const QString& filename) {
     }
 
     std::string title;
+    std::string title_long;
     system.GetAppLoader().ReadTitle(title);
+    system.GetAppLoader().ReadTitleLong(title_long);
     game_title = QString::fromStdString(title);
+    game_title_long = QString::fromStdString(title_long);
     UpdateWindowTitle();
 
     game_path = filename;
@@ -3028,14 +3031,14 @@ void GMainWindow::OnMoviePlaybackCompleted() {
 void GMainWindow::UpdateWindowTitle() {
     const QString full_name = QString::fromUtf8(Common::g_build_fullname);
 
-    if (game_title.isEmpty()) {
+    if (game_title_long.isEmpty()) {
         setWindowTitle(QStringLiteral("Citra %1").arg(full_name));
     } else {
-        setWindowTitle(QStringLiteral("Citra %1 | %2").arg(full_name, game_title));
+        setWindowTitle(QStringLiteral("Citra %1 | %2").arg(full_name, game_title_long));
         render_window->setWindowTitle(
-            QStringLiteral("Citra %1 | %2 | %3").arg(full_name, game_title, tr("Primary Window")));
-        secondary_window->setWindowTitle(QStringLiteral("Citra %1 | %2 | %3")
-                                             .arg(full_name, game_title, tr("Secondary Window")));
+            QStringLiteral("Citra %1 | %2 | %3").arg(full_name, game_title_long, tr("Primary Window")));
+        secondary_window->setWindowTitle(
+            QStringLiteral("Citra %1 | %2 | %3").arg(full_name, game_title_long, tr("Secondary Window")));
     }
 }
 