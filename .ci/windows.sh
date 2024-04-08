77b3ba1ef34dfc760500b01dcab44365e924b3f3
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-04-08 08:03:44 -0600

    Optimize builds with O2 CFLAGS

--- .ci/windows.sh
+++ .ci/windows.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -ex
 
+if [ "$TARGET" = "msvc" ]; then 
+    export EXTRA_CMAKE_FLAGS=(-DCMAKE_CXX_FLAGS="/O2" -DCMAKE_C_FLAGS="/O2")
+else
+    export EXTRA_CMAKE_FLAGS=(-DCMAKE_CXX_FLAGS="-O2" -DCMAKE_C_FLAGS="-O2")
+fi
+
 mkdir build && cd build
 cmake .. -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
