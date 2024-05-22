c142c710b313962bc5f658df5f378cc68297c58b
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-05-22 10:57:57 -0600

    Android: Update various dependencies

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("de.undercouch.download") version "5.6.0"
     id("kotlin-parcelize")
-    kotlin("plugin.serialization") version "1.9.24"
+    kotlin("plugin.serialization") version "2.0.0"
     id("androidx.navigation.safeargs.kotlin")
 }
 
