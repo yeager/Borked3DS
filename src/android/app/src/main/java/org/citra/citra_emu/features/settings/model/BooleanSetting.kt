7311bf219ff6bc788f1f16a280bb84a024f5288b
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-07-21 11:57:03 -0600

    Fix merge error

--- src/android/app/src/main/java/org/citra/citra_emu/features/settings/model/BooleanSetting.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/features/settings/model/BooleanSetting.kt
@@ -15,9 +15,9 @@ enum class BooleanSetting(
     PLUGIN_LOADER("plugin_loader", Settings.SECTION_SYSTEM, false),
     ALLOW_PLUGIN_LOADER("allow_plugin_loader", Settings.SECTION_SYSTEM, true),
     SWAP_SCREEN("swap_screen", Settings.SECTION_LAYOUT, false),
+    INSTANT_DEBUG_LOG("instant_debug_log", Settings.SECTION_DEBUG, false),
     RELAXED_PRECISION_DECORATORS("relaxed_precision_decorators", Settings.SECTION_RENDERER, false),
     ADRENO_GPU_BOOST("adreno_gpu_boost", Settings.SECTION_RENDERER, false);
-    INSTANT_DEBUG_LOG("instant_debug_log", Settings.SECTION_DEBUG, false);
 
     override var boolean: Boolean = defaultValue
 
