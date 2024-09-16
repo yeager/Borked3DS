e2bc4e2c24ad22b3e128116add3af14c017a34f2
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-09-16 15:02:19 -0600

    Android: Update signing key

--- .ci/android.sh
+++ .ci/android.sh
@@ -2,9 +2,9 @@
 
 export NDK_CCACHE=$(which ccache)
 
-if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
-    export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
-    base64 --decode <<< "${ANDROID_KEYSTORE_B64}" > "${ANDROID_KEYSTORE_FILE}"
+if [ ! -z "${DROID_KEYSTORE_B64}" ]; then
+    export DROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
+    base64 --decode <<< "${DROID_KEYSTORE_B64}" > "${DROID_KEYSTORE_FILE}"
 fi
 
 # Build Vulkan-ValidationLayers
@@ -24,6 +24,6 @@ chmod +x ./gradlew
 
 ccache -s -v
 
-if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
-    rm "${ANDROID_KEYSTORE_FILE}"
+if [ ! -z "${DROID_KEYSTORE_B64}" ]; then
+    rm "${DROID_KEYSTORE_FILE}"
 fi
