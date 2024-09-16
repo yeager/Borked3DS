014872158ebc3755678d6a65f71c47ef3c294526
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-09-16 14:56:17 -0600

    ci: Clean up options

--- .ci/android.sh
+++ .ci/android.sh
@@ -4,12 +4,11 @@ export NDK_CCACHE=$(which ccache)
 
 if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
     export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
-    base64 --decode <<< "${DROID_KEYSTORE_B64}" > "${ANDROID_KEYSTORE_FILE}"
+    base64 --decode <<< "${ANDROID_KEYSTORE_B64}" > "${ANDROID_KEYSTORE_FILE}"
 fi
 
-mkdir -p src/android/app/build/tmp
-
 # Build Vulkan-ValidationLayers
+mkdir -p src/android/app/build/tmp
 cd externals/Vulkan-ValidationLayers
 python3 scripts/android.py --config Release --app-abi 'arm64-v8a x86_64' --app-stl c++_static
 cd build-android/libs
