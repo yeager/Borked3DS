b7be7d1221348ac9d0c5f4d37a0673744e43ec37
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-11-27 21:21:03 -0700

    Android: Kotlin 2.1.0

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("de.undercouch.download") version "5.6.0"
     id("kotlin-parcelize")
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("plugin.serialization") version "2.1.0"
     id("androidx.navigation.safeargs.kotlin")
 }
 
--- src/android/build.gradle.kts
+++ src/android/build.gradle.kts
@@ -7,8 +7,8 @@
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("com.android.library") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
 }
 
 tasks.register("clean").configure {
