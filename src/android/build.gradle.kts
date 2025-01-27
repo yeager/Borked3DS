85fac894821ee62422974bc459f8092066efc661
Reg Tiangha <rtiangha@users.noreply.github.com>
2025-01-27 15:00:16 -0700

    Android: Kotlin 2.1.10

--- src/android/build.gradle.kts
+++ src/android/build.gradle.kts
@@ -7,8 +7,8 @@
 plugins {
     id("com.android.application") version "8.8.0" apply false
     id("com.android.library") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
 }
 
 tasks.register<Delete>("clean") {
